0a81f92327563dfb62bae7adc1ab3722397022fc
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-07-22 15:58:00 -0600

    commit tree 7dce706dbbb466139c87215f5c44e56bf7656476

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db 
+    end
+
+    def self.save(name, type, db)
+        sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+        @id = db.execute(sql, name, type)
+    end
+
+    def self.find(id_num, db)
+        poke_info = db.execute("SELECT * FROM pokemon WHERE id = ?", id_num).flatten
+        Pokemon.new(id: poke_info[0], name: poke_info[1], type: poke_info[2], db: db)
+    end
 end
